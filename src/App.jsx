7c261dad5b760b5c7941a89a04d85f32e4fc48a3
juantoranzos <toranzos628@gmail.com>
2023-05-22 13:13:03 -0300

    realice el punto uno del tp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Titulo from './components/HelloWorld'
+import { Container } from 'react-bootstrap'
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <HelloWorld> </HelloWorld>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const HelloWorld = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      
+    </>
+  );
+}
+
+export default HelloWorld;
